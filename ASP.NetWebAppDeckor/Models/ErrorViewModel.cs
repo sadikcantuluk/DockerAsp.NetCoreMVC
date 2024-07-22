007b4636ae2d03f920dad82d47822607cbe477aa
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-07-22 16:56:36 +0300

    .Net CLI komutları da kullanılarak Asp .Net Core MVC projesi dockerize edildi.

---
+++ ASP.NetWebAppDeckor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NetWebAppDeckor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
